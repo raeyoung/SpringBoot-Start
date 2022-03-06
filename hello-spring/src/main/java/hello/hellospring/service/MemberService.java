0fa7d7abebe931a99c170adfe743b18d031e4c80
raeyoung <raeyoung233@gmail.com>
2022-03-06 20:59:37 +0900

    AOP 사용

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -28,17 +28,18 @@ public class MemberService {
      * 회원가입
      */
     public Long join(Member member) {
+        // 아래와 같이 축약하여 사용 가능
+        validateDuplicateMember(member); // 중복 회원 검증
+
+        memberRepository.save(member);
+        return member.getId();
         // 같은 이름이 있는 중복 회원 X
         /*Optional<Member> result = memberRepository.findByName(member.getName());
         // Null이 아닌 값이 존재한다면 (optional 이 있기 때문에 사용 가능)
         result.ifPresent(m -> {
             throw new IllegalStateException("이미 존재하는 회원입니다.");
         });*/
-        // 아래와 같이 축약하여 사용 가능
-        validateDuplicateMember(member); // 중복 회원 검증
 
-        memberRepository.save(member);
-        return member.getId();
     }
 
     private void validateDuplicateMember(Member member) {
@@ -52,6 +53,7 @@ public class MemberService {
      * 전체 회원 조회
      */
     public List<Member> findMembers() {
+
         return memberRepository.findAll();
     }
 
